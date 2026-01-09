443792e08a9be2bd5c963dc35ceb382f8d6c2026
BongaGougota0 <Bonga_gougota@outlook.com>
2026-01-09 16:07:03 +0200

    insert starter file for demo data generator, create global class to handle errors.

---
+++ src/main/java/za/co/teama/data/pulse/Controller/GlobalErrorHandler.java
@@ -0,0 +1,9 @@
+package za.co.teama.data.pulse.Controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalErrorHandler {
+
+
+}
---
+++ src/main/java/za/co/teama/data/pulse/config/DemoGenerator.java
@@ -0,0 +1,10 @@
+package za.co.teama.data.pulse.config;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Use to generate demo data
+ */
+@Component
+public class DemoGenerator {
+}
