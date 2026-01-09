9beaaa4362d9928f68c8d4a5eb73dcbad88a68e3
Sifiso <tsheposifisomoloi@gmail.com>
2026-01-09 09:38:17 +0200

    New changes

--- src/main/java/za/co/teama/data/pulse/Controller/UserController.java
+++ src/main/java/za/co/teama/data/pulse/Controller/UserController.java
@@ -1,16 +1,24 @@
 package za.co.teama.data.pulse.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import za.co.teama.data.pulse.Service.UserService;
+import za.co.teama.data.pulse.Models.User;
 
 @RestController
 @RequestMapping ("/api/users")
 public class UserController {
-
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping
+    public String register(@RequestBody User user) {
+        userService.createUser(user);
+        return "Registered successfully";
     }
 }
--- src/main/java/za/co/teama/data/pulse/Service/UserService.java
+++ src/main/java/za/co/teama/data/pulse/Service/UserService.java
@@ -1,6 +1,7 @@
 package za.co.teama.data.pulse.Service;
 
 import org.springframework.stereotype.Service;
+import za.co.teama.data.pulse.Models.User;
 import za.co.teama.data.pulse.Repository.UserRepository;
 
 @Service
@@ -9,5 +10,10 @@ public class UserService {
 
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        userRepository.save(user);
+        return user;
     }
 }
