ded734871bf1ad78d5fb7b2b44c8d7d6375be128
Thando <thandwem045@gmail.com>
2026-01-09 09:04:48 +0200

    Created survey file

--- src/main/java/za/co/teama/data/pulse/Controller/SurveyController.java
+++ src/main/java/za/co/teama/data/pulse/Controller/SurveyController.java
@@ -1,18 +1,27 @@
 package za.co.teama.data.pulse.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import za.co.teama.data.pulse.Service.SurveyService;
+import za.co.teama.data.pulse.Service.SurveysService;
 
 
 //Gets all surveys
 @RestController
 @RequestMapping("/api/survey")
 public class SurveyController {
-    private final SurveyService surveyService;
 
-    public SurveyController(SurveyService surveyService) {
-        this.surveyService = surveyService;
+    @Autowired
+    private final SurveysService surveysService;
+
+    public SurveyController(SurveysService surveyService) {
+        this.surveysService = surveyService;
+    }
+
+    @GetMapping("/surveys")
+    private Object getAllSurveys() {
+       return surveysService.getSurveys();
     }
 }
 
--- src/main/java/za/co/teama/data/pulse/Repository/SurveyRepository.java
+++ src/main/java/za/co/teama/data/pulse/Repository/SurveyRepository.java
@@ -4,8 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import za.co.teama.data.pulse.Models.Survey;
 
-public interface SurveyRepository {
-    @Repository
-    public interface surveyRepository extends JpaRepository<Survey, Long> {
-    }
-}
+@Repository
+public interface SurveyRepository extends JpaRepository<Survey, Long> {}
