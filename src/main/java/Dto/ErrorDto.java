2a8898d783cdfd2ad50a6be60c225d79cca9e655
BongaGougota0 <Bonga_gougota@outlook.com>
2026-01-12 11:04:01 +0200

    implement custom error handling, create error dto to send error message, implement controller advice for error handling

---
+++ src/main/java/Dto/ErrorDto.java
@@ -0,0 +1,13 @@
+package Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDto {
+    private String message;
+    private String statusCode;
+}
