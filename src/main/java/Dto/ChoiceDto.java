cab1307a868762affebdf46a05ce994807defcf5
BongaGougota0 <Bonga_gougota@outlook.com>
2026-01-14 09:10:10 +0200

    fix serialization error for the new dto [ChoiceDto and QuestionDto], solution add @Data annotation and @AllArgs and @NoArgs where necessary.

--- src/main/java/Dto/ChoiceDto.java
+++ src/main/java/Dto/ChoiceDto.java
@@ -1,8 +1,12 @@
 package Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import za.co.teama.data.pulse.Models.Choice;
 import za.co.teama.data.pulse.Models.Question;
 
+@Data
+@AllArgsConstructor
 public class ChoiceDto {
     private Integer id;
     private String optionText;
--- src/main/java/Dto/QuestionDto.java
+++ src/main/java/Dto/QuestionDto.java
@@ -1,21 +1,16 @@
 package Dto;
 
-import za.co.teama.data.pulse.Models.Choice;
-import za.co.teama.data.pulse.Models.Survey;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class QuestionDto {
     private Integer id;
     private String text;
     private List<ChoiceDto> choiceOptions;
-
-    public QuestionDto(Integer id, String text, List<ChoiceDto> choiceOptions) {
-        this.id = id;
-        this.text = text;
-        this.choiceOptions = choiceOptions;
-    }
-
-    public QuestionDto() {}
 
 }
